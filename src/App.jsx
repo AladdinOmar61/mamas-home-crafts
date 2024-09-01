4641091292a4bf645bf2fef8cd800884a3d69631
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-09-01 00:36:30 -0400

    added title and 3 images for mini gallery/feature section
    next, will work on media queries for the few elements already rendered for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Mona Store</h1>
+      {/* These will be images in the future */}
+      <div className='photo-container'>
+        <img style={{width: '33rem', height: '33rem', border: 'solid black 1px'}}/>
+        <img style={{width: '33rem', height: '33rem', border: 'solid black 1px'}}/>
+        <img style={{width: '33rem', height: '33rem', border: 'solid black 1px'}}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
