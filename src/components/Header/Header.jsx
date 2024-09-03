c2ab605f4a3d0c933adffe24552877cca9d07023
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-09-02 20:30:51 -0400

    added more design changes
    - imported some icons
    - made some design changes to header
    - fixed some default styling to be compatible with other operating systems

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
+import './Header.css';
+import Icon from '@mdi/react';
+import { mdiCartOutline } from '@mdi/js';
+import { mdiAccountOutline } from '@mdi/js';
+
+
 function Header() {
   return (
     <>
       <div className="header">
-        <p>profile</p>
-        <h1>Made by Mama</h1>
-        <p>cart</p>
+        <h1 className="website-title">Made by Mama</h1>
+        <div className='icons'>
+        <Icon className='cart' path={mdiCartOutline} size={1.5} />
+        <Icon className='account' path={mdiAccountOutline} size={1.5} />
+        </div>
       </div>
     </>
   );
