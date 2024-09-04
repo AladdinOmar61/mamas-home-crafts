9323ca349f6519ca235789a8eb72c112848ac3da
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-09-03 20:10:34 -0400

    cleaned up featureGallery.jsx

--- src/components/FeatureGallery/FeatureGallery.jsx
+++ src/components/FeatureGallery/FeatureGallery.jsx
@@ -1,88 +1,33 @@
-// import "./FeatureGallery.css";
-// import plateStand from "../../assets/images/plateOnStand.jpg";
-// import pumpkin from "../../assets/images/pumpkin.jpg";
-// import platePumpkin from "../../assets/images/platePumpkinStand.jpg";
-// import { useEffect, useState } from "react";
-
-// function FeatureGallery() {
-//   const images = [plateStand, pumpkin, platePumpkin];
-
-//   const [currIdx, setCurrIdx] = useState(0);
-//   const delay = 3000;
-
-//   const next = () => {
-//     setCurrIdx((prevIdx) => (prevIdx + 1) % images.length);
-//   };
-
-//   const prev = () => {
-//     setCurrIdx((prevIdx) => (prevIdx - 1 + images.length) % images.length);
-//   };
-
-//   useEffect(() => {
-//     const interval = setInterval(next, delay);
-//     return () => clearInterval(interval);
-//   }, [currIdx]);
-
-//   return (
-//     <>
-//       <div className="feature-container">
-//         <button onClick={prev}>&#10094;</button>
-//         {/* TODO: add actual images once we get them */}
-//         {images.map((image, index) => (
-//           <div
-//             className={`"feature-img" ${index === currIdx ? "active" : ""}`}
-//             key={index}
-//           >
-//           <img src={image} />
-//           </div>
-//         ))}
-//         <button onClick={next}>&#10095;</button>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default FeatureGallery;
-
 import { useState, useEffect } from "react";
 import "./FeatureGallery.css"; // Import CSS for styling
 import plateStand from "../../assets/images/plateOnStand.jpg";
 import pumpkin from "../../assets/images/pumpkin.jpg";
 import platePumpkin from "../../assets/images/platePumpkinStand.jpg";
 
-const Carousel = () => {
-  const images = [plateStand, pumpkin, platePumpkin, pumpkin, pumpkin];
+const FeatureGallery = () => {
+  const images = [plateStand, pumpkin, platePumpkin, plateStand, pumpkin];
 
   const [currentIndex, setCurrentIndex] = useState(0);
-  const delay = 3000; // Auto-scroll delay in milliseconds
+  const delay = 3000;
   const visibleImages = 3
 
-  // Function to go to the next slide
   const goToNext = () => {
     if (images.length > 3) {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % (images.length-2));
     }
-    // if (currentIndex < images.length - visibleImages) {
-    //   setCurrentIndex(currentIndex + 1);
-    // }
   };
 
-  // Function to go to the previous slide
   const goToPrevious = () => {
     if (images.length > 3) {
       setCurrentIndex(
         (prevIndex) => (prevIndex - 1 + (images.length-2)) % (images.length-2)
       );
     }
-    // if (currentIndex > 0) {
-    //   setCurrentIndex(currentIndex - 1);
-    // }
   };
 
-  // Auto-scroll effect using useEffect
   useEffect(() => {
     const interval = setInterval(goToNext, delay);
-    return () => clearInterval(interval); // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, [currentIndex]);
 
   return (
@@ -114,4 +59,4 @@ const Carousel = () => {
   );
 };
 
-export default Carousel;
+export default FeatureGallery;
