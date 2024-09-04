1558ac9d6c1e8cc6f5f1430c55164855d7e5bf0f
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-09-04 19:59:28 -0400

    added conditional rendering based on screen width
    imported a window size hook that dynamically keeps track of screen size. Next, will add the new render to the info section based on a lower screen width

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -3,27 +3,29 @@ import Icon from "@mdi/react";
 import { mdiMotherHeart } from "@mdi/js";
 import { mdiPackageVariantClosed } from "@mdi/js";
 import { mdiTruckDelivery } from "@mdi/js";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 function InfoSection() {
+  const size = useWindowSize();
   return (
     <>
       <div className="info-section">
+        {size.width <= 650 ? ( <h1>Why Choose Us?</h1> ) : (
+          <>
         <div className="info-bit" style={{ color: "white" }}>
-          <Icon path={mdiMotherHeart} size={5}  />
+          <Icon path={mdiMotherHeart} className="info-icon" />
           <p>Made with love and care by mama</p>
         </div>
         <div className="info-bit" style={{ color: "white" }}>
-          <Icon
-            path={mdiPackageVariantClosed}
-            size={5}
-            
-          />
+          <Icon path={mdiPackageVariantClosed} className="info-icon" />
           <p>Packaged securely and precisely</p>
         </div>
         <div className="info-bit" style={{ color: "white" }}>
-          <Icon path={mdiTruckDelivery} size={5}  />
+          <Icon path={mdiTruckDelivery} className="info-icon" />
           <p>Delivered with speed and safety to your doorstep</p>
         </div>
+        </> 
+         )}
       </div>
     </>
   );
