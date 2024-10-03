e6a32e92ea57a75015c3b905bb58f13e5866ad19
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-10-03 19:55:44 -0400

    made a basic products page that shows a gallery of images to be sold

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,22 +1,26 @@
-import './Products.css'
+import "./Products.css";
 import Header from "../../components/Header/Header";
+import plateStand from "../../assets/images/plateOnStand.jpg";
+import pumpkin from "../../assets/images/pumpkin.jpg";
+import platePumpkin from "../../assets/images/platePumpkinStand.jpg";
 
 function Products() {
+  const Prodimages = [
+    plateStand,
+    pumpkin,
+    platePumpkin,
+    plateStand,
+    pumpkin,
+  ];
+
   return (
     <div className="products-page">
       <Header />
       <h1 className="products-header">Products</h1>
-      <div className='product-container'>
-      <div className='product-gallery'>
-        <div className='product-item'>1</div>
-        <div className='product-item'>2</div>
-        <div className='product-item'>3</div>
-        <div className='product-item'>4</div>
-        <div className='product-item'>5</div>
-        {/* <div className='product-item'>6</div> */}
-        {/* <div className='product-item'>7</div> */}
-        {/* <div className='product-item'>8</div> */}
-      </div>
+      <div className="product-gallery">
+        {Prodimages.map((img, index) => (
+          <img key={index} className="product-item" src={img} />
+        ))}
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,10 +20,14 @@ function Header() {
   return (
     <div className="header">
       <div className="header-nav">
-      <Link to="/" className="website-title" style={{ textDecoration: "none" }}>
-        Made by Mama
-      </Link>
-      <Link to="/products" className="products">Products</Link>
+        <Link to="/" className="website-title">
+          Made by Mama
+        </Link>
+        {loggedIn && (
+          <Link to="/products" className="products">
+            Products
+          </Link>
+        )}
       </div>
       {loggedIn ? (
         <div className="icons">
@@ -86,17 +90,15 @@ function Header() {
               <p>Name of the product</p>
               <p>$59.00</p>
               <div className="cart-item-quantity">
-              <button className="subtract-item">-</button>
-              <p>0</p>
-               <button className="add-item">+</button>
+                <button className="subtract-item">-</button>
+                <p>0</p>
+                <button className="add-item">+</button>
               </div>
               <a href="">remove</a>
             </div>
           </div>
         </div>
-        <button className="checkout">
-          Checkout
-        </button>
+        <button className="checkout">Checkout</button>
       </ReactModal>
     </div>
   );
