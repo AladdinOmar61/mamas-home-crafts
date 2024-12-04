8fe578b739ab84936bc4d67ae89554bb02d75544
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2024-12-03 19:00:05 -0500

    updated file name for vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from 'react'
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import { SupabaseProvider } from "../lib/context/supabaseProvider";
+import { SupabaseProvider } from "../lib/context/SupabaseProvider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Register from "./pages/Register/register.jsx";
